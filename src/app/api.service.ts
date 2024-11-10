8d1e8e7e813bb6573767fb0fd01451e4c6de59e4
Thant Zin Lin <tzlin@mit.com.mm>
2024-11-10 22:15:10 +0630

    Update latest

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,32 +1,33 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
-  private baseurl = 'http://localhost:5000/api/';
+  private apiurl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
   getAll(url: string, body: any) {
-    return this.http.post<any>(`${this.baseurl}${url}`, body);
+    return this.http.post<any>(`${this.apiurl}${url}`, body);
   }
 
   get(url: string): Observable<any> {
-    return this.http.get<any>(`${this.baseurl}${url}`);
+    return this.http.get<any>(`${this.apiurl}${url}`);
   }
 
   post(url: string, body: any): Observable<any> {
-    return this.http.post<any>(`${this.baseurl}${url}`, body);
+    return this.http.post<any>(`${this.apiurl}${url}`, body);
   }
 
   put(url: string, body: any): Observable<any> {
-    return this.http.put<any>(`${this.baseurl}${url}`, body);
+    return this.http.put<any>(`${this.apiurl}${url}`, body);
   }
 
   delete(url: string): Observable<any> {
-    return this.http.delete<any>(`${this.baseurl}${url}`);
+    return this.http.delete<any>(`${this.apiurl}${url}`);
   }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,87 +12,92 @@ import { Product } from '../model/product.model';
 export class HomeComponent implements OnInit {
   targetDate: Date = new Date('2023-10-18T00:00:00'); // Replace with your flash sale end date
   countdown: string = '';
-  // products: any[] = [];
+  products: any[] = [];
   pagObj = { page: 1, limit: 10, totalPages: 1, count: 0, search: '' };
-  products: Product[] = [
-    {
-      id: 1,
-      name: 'Coca-Cola',
-      description: 'Product description goes here.',
-      price: 19.99,
-      image: 'assets/images/img1.jpg',
-      quantity: 1,
-      // subtotal: 19.99,
-    },
-    {
-      id: 2,
-      name: 'Product 2',
-      description: 'Another product description.',
-      price: 29.99,
-      image: 'assets/images/img2.jpg',
-      quantity: 1,
-      // subtotal: 29.99,
-    },
-    {
-      id: 3,
-      name: 'Camera',
-      description: 'Yet another product description.',
-      price: 9.99,
-      image: 'assets/images/img3.jpg',
-      quantity: 1,
-      // subtotal: 9.99,
-    },
-    {
-      id: 4,
-      name: 'Camera',
-      description: 'Yet another product description.',
-      price: 9.99,
-      image: 'assets/images/img3.jpg',
-      quantity: 1,
-      // subtotal: 9.99,
-    },
-    {
-      id: 5,
-      name: 'Camera',
-      description: 'Yet another product description.',
-      price: 9.99,
-      image: 'assets/images/img3.jpg',
-      quantity: 1,
-      // subtotal: 9.99,
-    },
-    {
-      id: 6,
-      name: 'Camera',
-      description: 'Yet another product description.',
-      price: 9.99,
-      image: 'assets/images/img3.jpg',
-      quantity: 1,
-      // subtotal: 9.99,
-    },
-    {
-      id: 7,
-      name: 'Camera',
-      description: 'Yet another product description.',
-      price: 9.99,
-      image: 'assets/images/img3.jpg',
-      quantity: 1,
-      // subtotal: 9.99,
-    },
-    {
-      id: 8,
-      name: 'Camera',
-      description: 'Yet another product description.',
-      price: 9.99,
-      image: 'assets/images/img3.jpg',
-      quantity: 1,
-      // subtotal: 9.99,
-    },
-  ];
-  constructor(private router: Router, private cartService: CartService) {}
+  // products: Product[] = [
+  //   {
+  //     id: 1,
+  //     name: 'Coca-Cola',
+  //     description: 'Product description goes here.',
+  //     price: 19.99,
+  //     image: 'assets/images/img1.jpg',
+  //     quantity: 1,
+  //     // subtotal: 19.99,
+  //   },
+  //   {
+  //     id: 2,
+  //     name: 'Product 2',
+  //     description: 'Another product description.',
+  //     price: 29.99,
+  //     image: 'assets/images/img2.jpg',
+  //     quantity: 1,
+  //     // subtotal: 29.99,
+  //   },
+  //   {
+  //     id: 3,
+  //     name: 'Camera',
+  //     description: 'Yet another product description.',
+  //     price: 9.99,
+  //     image: 'assets/images/img3.jpg',
+  //     quantity: 1,
+  //     // subtotal: 9.99,
+  //   },
+  //   {
+  //     id: 4,
+  //     name: 'Camera',
+  //     description: 'Yet another product description.',
+  //     price: 9.99,
+  //     image: 'assets/images/img3.jpg',
+  //     quantity: 1,
+  //     // subtotal: 9.99,
+  //   },
+  //   {
+  //     id: 5,
+  //     name: 'Camera',
+  //     description: 'Yet another product description.',
+  //     price: 9.99,
+  //     image: 'assets/images/img3.jpg',
+  //     quantity: 1,
+  //     // subtotal: 9.99,
+  //   },
+  //   {
+  //     id: 6,
+  //     name: 'Camera',
+  //     description: 'Yet another product description.',
+  //     price: 9.99,
+  //     image: 'assets/images/img3.jpg',
+  //     quantity: 1,
+  //     // subtotal: 9.99,
+  //   },
+  //   {
+  //     id: 7,
+  //     name: 'Camera',
+  //     description: 'Yet another product description.',
+  //     price: 9.99,
+  //     image: 'assets/images/img3.jpg',
+  //     quantity: 1,
+  //     // subtotal: 9.99,
+  //   },
+  //   {
+  //     id: 8,
+  //     name: 'Camera',
+  //     description: 'Yet another product description.',
+  //     price: 9.99,
+  //     image: 'assets/images/img3.jpg',
+  //     quantity: 1,
+  //     // subtotal: 9.99,
+  //   },
+  // ];
+  constructor(
+    private router: Router,
+    private cartService: CartService,
+    private apiservice: ApiService,
+    private toast: ToastrService
+  ) {}
 
   ngOnInit(): void {
     this.updateCountdown();
-    // this.fetchProducts();
+    this.fetchProducts();
     setInterval(() => {
       this.updateCountdown();
     }, 1000);
@@ -144,10 +149,11 @@ export class HomeComponent implements OnInit {
 
   // async fetchProducts() {
   //   try {
-  //     const res: any = await this.apiservice
-  //       .getAll(`product/getall`, this.pagObj)
-  //       .toPromise();
+  //     const res: any = this.apiservice.get(
+  //       `products?page=1&perPage=10&search=`
+  //     );
   //     if (res) {
+  //       console.log('response data:' + JSON.stringify(res));
   //       this.products = res.data;
   //       this.pagObj.totalPages = res.totalPages;
   //       this.pagObj.count = res.count;
@@ -156,6 +162,21 @@ export class HomeComponent implements OnInit {
   //     console.error('An error occurred while fetching products:', error);
   //   }
   // }
+
+  async fetchProducts() {
+    try {
+      const res: any = await this.apiservice.get(
+        `products?page=1&perPage=10&search=`
+      ).subscribe;
+      if (res) {
+        this.products = res.data;
+      }
+    } catch (error) {
+      this.toast.error('An error occurred while fetching products', 'Error');
+
+      console.error('An error occurred while fetching products', error);
+    }
+  }
 
   addToCart(product: Product) {
     this.cartService.addToCart(product);
