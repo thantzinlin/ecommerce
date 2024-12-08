439dea4846693a86e622bb693ac5a03e4c48e8e8
Thant Zin Lin <tzlin@mit.com.mm>
2024-12-08 14:31:02 +0630

    update env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,9 @@ export const environment = {
   production: false,
 
   //  apiUrl: 'http://localhost:5000/api',
-  apiUrl:
-    'https://ecomm-api-eedbh0fnfmarhsbr.southeastasia-01.azurewebsites.net/api',
+  apiUrl: 'http://104.43.56.158:5000/api',
+  // apiUrl:
+  //   'https://ecomm-api-eedbh0fnfmarhsbr.southeastasia-01.azurewebsites.net/api',
 
   socketUrl: 'http://localhost:5000',
 };
